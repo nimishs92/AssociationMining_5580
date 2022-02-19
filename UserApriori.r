6d88534684cb7e83d587f0bc6f3fd6e6e6c5d364
Imtiyaz_A00456832 <90959435+A00456832@users.noreply.github.com>
2022-02-18 22:34:06 -0400

    Updated transpose logic

--- UserApriori.r
+++ UserApriori.r
@@ -1,46 +1,57 @@
 library(arules)
 library(plyr)
+library(dplyr)
+library(ggplot2)
 
-df_user= read.csv("./user.csv")
-df_user = ddply(df_user,c("id"),function(dfl)paste(dfl$milestone, collapse = ","))
-df_user$id = NULL
-write.table(df_user,"./Milestones2.csv", quote=FALSE, row.names = FALSE, col.names = FALSE)
-tr = read.transactions("./Milestones2.csv",format="basket",sep=",")
+
+install.packages("dplyr")
+install.packages("ggplot2")
+
+df_user= read.csv("Clean_UserData.csv")
+summary(df_user)
+
+print (df_user)
+
+# # Graph- 1
+# df_user_Analysis <- df_user%>%
+#   dplyr::group_by(milestone_name)%>%
+#   dplyr::tally()%>%
+#   dplyr::mutate(percent=n/sum(n))
+# 
+# pl <- ggplot(data = top_n(df_user_Analysis, 5, n),aes(x= reorder(milestone_name, -n), y = n))
+# pl <- pl + geom_bar(stat="identity")
+# pl <- pl + geom_text(aes(label=paste0(sprintf("%1.1f", percent*100),"%", " (",n,")")), position=position_stack(vjust=0.5), colour="white", size = 3)
+# pl <- pl + labs(x="Milestones", y="Count", title = "Milestone visiting details")
+# pl
+# 
+# # Graph- 2
+# pl_bottom <- ggplot(data = top_n(df_user_Analysis, -5, n),aes(x= reorder(milestone_name, n), y = n))
+# pl_bottom <- pl_bottom + geom_bar(stat="identity")
+# pl_bottom <- pl_bottom + geom_text(aes(label=paste0(sprintf("%1.1f", percent*100),"%", " (",n,")")), position=position_stack(vjust=0.5), colour="white", size = 3)
+# pl_bottom <- pl_bottom + labs(x="Milestones", y="Count", title = "Milestone visiting details")
+# pl_bottom
+
+
+# Transpose
+
+df_user_New <- df_user[c("user_id","milestone_name")]
+print(df_user_New)
+
+df_user_transpose = ddply(df_user_New,c("user_id"),function(dfl)paste(dfl$milestone_name, collapse=","))
+head(df_user_transpose)
+#print(df_user_transpose)
+df_user_transpose$user_id = NULL
+#head(df_user_transpose)
+write.table(df_user_transpose,"./Milestones2_I.csv", quote=FALSE, row.names = FALSE, col.names = FALSE)
+tr = read.transactions("./Milestones2_I.csv",format="basket",sep=",")
 summary(tr)
+
 itemFrequencyPlot(tr, topN=10)
-rules = apriori(tr,parameter = list(supp=0.4,conf=0.6))
-inspect(rules)
-inspect(sort(rules,by='lift'))
+
+#Try 1
+rules = apriori(tr,parameter = list(supp=0.3,conf=0.8))
+inspect(sort(rules,by='lift'))[1:10]
 itemsets=unique(generatingItemsets(rules))
 itemsets
 inspect(itemsets)
-
-
-rules = apriori(tr,parameter = list(supp=0.3,conf=0.9))
-rules=sort(rules,by='lift')
-subset.matrix = is.subset(rules, rules)
-precolumn=colSums(subset.matrix, na.rm=T) 
-
-#REMOVE SUB RULES RULE, Consider only Main rule
-x=subset.matrix@Dimnames[[1]]
-subset.matrix[lower.tri(subset.matrix, diag=T)] = 0 
-column=colSums(subset.matrix, na.rm=T) 
-notredundant=c()
-for(i in 1:length(column)){
-  if(column[i]<1){
-    if(precolumn[i]-column[i]==1){
-      print(i)
-      notredundant[i]=TRUE
-    }
-    else{
-      notredundant[i]=FALSE
-    }
-  }
-  else{
-      notredundant[i]=TRUE
-  }
-}
-rules=rules[notredundant]
-rules=sort(rules,by='lift')
-inspect(rules)
 
